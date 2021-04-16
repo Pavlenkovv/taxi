635608c1e7c540e4322103874ec7b04adae31b93
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-04-16 13:18:53 +0300

    models done

--- main/models.py
+++ main/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    CUSTOMER = 'customer'
+    DISPATCHER = 'dispatcher'
+    USER_TYPES = (
+        (CUSTOMER, 'Customer'),
+        (DISPATCHER, 'Dispatcher'),
+    )
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    phone = models.CharField(blank=False, max_length=30)
+    type = models.CharField(max_length=30, blank=False, choices=USER_TYPES)
+
+    # @property
+    # def __str__(self):
+    #     return self.first_name, self.last_name
+
+
+class Order(models.Model):
+    customer_name = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    customer_phone = models.CharField(max_length=30, null=False, blank=False)
+    address_from = models.CharField(max_length=200, null=False, blank=False)
+    address_to = models.CharField(max_length=200, null=False, blank=False)
+    in_what_time = models.CharField(max_length=50, null=False, blank=False)
+    car = models.ForeignKey('Car', on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return self.customer_name
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=50)
+    number = models.CharField(max_length=50)
+
+    @property
+    def is_ordered(self):
+        return hasattr(self, 'orders')
+
+    def __str__(self):
+        return self.brand
