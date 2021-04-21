9e645fc0a2fa75f4aa1f604b355919f23047baae
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-04-21 22:52:49 +0300

    taxi app version 1

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from main.views import *
 
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('orders/', order_list),
     path('orders/details', order_details),
     path('details', user_order_details),
